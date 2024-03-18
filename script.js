2e499f6a96398e2058782399054890041e34d4d0
willians10 <159455436+willians10@users.noreply.github.com>
2024-03-18 13:43:22 -0300

    Primeiro commit

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function changeColor {
-var text = document.getElementById('Title')
+function toggleMode() {
+  var text = document.getElementById("text")
+  if (text.classList.contains("white-text")) {
+    text.classList.remove("white-text")
+    text.classList.add("grey-text")
+  } else {
+    text.classList.remove("grey-text")
+    text.classList.add("white-text")
+  }
 }
-

45f38d5fcb49f3cbfa533c09deea3387fdbd411b
willians10 <159455436+willians10@users.noreply.github.com>
2024-03-18 13:13:55 -0300

    subindo novas funções

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function toggleMode() {
+async function toggleMode() {
   var text = document.getElementById("text")
   if (text.classList.contains("white-text")) {
     text.classList.remove("white-text")
@@ -7,4 +7,17 @@ function toggleMode() {
     text.classList.remove("grey-text")
     text.classList.add("white-text")
   }
+  const data = await searchProfileGitHub()
+  changeElement(data)
+}
+async function searchProfileGitHub() {
+  const response = await fetch("https://api.github.com/users/willians10")
+  const data = await response.json()
+  return data
+}
+function changeElement(data) {
+  var userElement = document.getElementById("user-github")
+  var textUser = document.createElement("p")
+  textUser.textContent = data.name
+  userElement.append(textUser)
 }
